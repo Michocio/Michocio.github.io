7923e64a1e4d9b2102ea4d3ab742ae503e9a9741
Michocio <micjaron@gmail.com>
2019-07-13 15:06:50 +0200

    xxhjjjawwjajjjjjjjjjaj

--- alls/1.js
+++ alls/1.js
@@ -1,11 +1,6 @@
 
 
-<style>
 
-</style>
-
-<div id="id1"></div>
-<script>
 function mpld3_load_lib(url, callback){
   var s = document.createElement('script');
   s.src = url;
@@ -604,4 +599,4 @@ if(typeof(mpld3) !== "undefined" && mpld3._mpld3IsLoaded){
             })
          });
 }
-</script>
+
